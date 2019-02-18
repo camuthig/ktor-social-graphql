8fb275fcf37141076dc5f67d03fa43aba02880a3
Chris Muthig <camuthig@gmail.com>
2019-02-18 18:41:52 +1030

    Initial commit of generate Ktor project
    Included in the generation was:
    * JWT authentication
    * OAuth authentication
    * GSON
    * Apache Client
    * Static content

---
+++ build.gradle.kts
@@ -0,0 +1,45 @@
+import org.jetbrains.kotlin.gradle.dsl.Coroutines
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+val ktor_version: String by project
+val kotlin_version: String by project
+val logback_version: String by project
+
+plugins {
+    application
+    kotlin("jvm") version "1.3.20"
+}
+
+group = "ktor-social-graphql"
+version = "0.0.1"
+
+application {
+    mainClassName = "io.ktor.server.netty.EngineMain"
+}
+
+repositories {
+    mavenLocal()
+    jcenter()
+    maven { url = uri("https://kotlin.bintray.com/ktor") }
+}
+
+dependencies {
+    compile("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlin_version")
+    compile("io.ktor:ktor-server-netty:$ktor_version")
+    compile("ch.qos.logback:logback-classic:$logback_version")
+    compile("io.ktor:ktor-server-core:$ktor_version")
+    compile("io.ktor:ktor-server-host-common:$ktor_version")
+    compile("io.ktor:ktor-auth:$ktor_version")
+    compile("io.ktor:ktor-auth-jwt:$ktor_version")
+    compile("io.ktor:ktor-gson:$ktor_version")
+    compile("io.ktor:ktor-client-core:$ktor_version")
+    compile("io.ktor:ktor-client-core-jvm:$ktor_version")
+    compile("io.ktor:ktor-client-apache:$ktor_version")
+    testCompile("io.ktor:ktor-server-tests:$ktor_version")
+}
+
+kotlin.sourceSets["main"].kotlin.srcDirs("src")
+kotlin.sourceSets["test"].kotlin.srcDirs("test")
+
+sourceSets["main"].resources.srcDirs("resources")
+sourceSets["test"].resources.srcDirs("testresources")
---
+++ src/Application.kt
@@ -0,0 +1,47 @@
+package org.camuthig
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.content.*
+import io.ktor.http.content.*
+import io.ktor.auth.*
+import io.ktor.gson.*
+import io.ktor.features.*
+import io.ktor.client.*
+import io.ktor.client.engine.apache.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    install(Authentication) {
+    }
+
+    install(ContentNegotiation) {
+        gson {
+        }
+    }
+
+    val client = HttpClient(Apache) {
+    }
+
+    routing {
+        get("/") {
+            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+        }
+
+        // Static feature. Try to access `/static/ktor_logo.svg`
+        static("/static") {
+            resources("static")
+        }
+
+        get("/json/gson") {
+            call.respond(mapOf("hello" to "world"))
+        }
+    }
+}
+
